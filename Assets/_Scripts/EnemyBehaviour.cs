bdf532072b6556e6d71e193d330b11ea91837867
VincentTseVC <from_vincent@hotmail.com>
2021-02-13 20:31:47 -0500

    added health System

--- Assets/_Scripts/EnemyBehaviour.cs
+++ Assets/_Scripts/EnemyBehaviour.cs
@@ -6,19 +6,40 @@ using UnityEngine.AI;
 public class EnemyBehaviour : MonoBehaviour
 {
 
+    public int maxHealth = 100;
+
+    public int currentHealth;
+
+
     public NavMeshAgent navMeshAgent;
 
     public Transform player;
+
+    public HealthBar healthBar;
 
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        healthBar.SetMaxHealth(maxHealth);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         navMeshAgent.SetDestination(player.position);
+
+        // testing
+        //if (Input.GetKeyDown(KeyCode.A))
+        //    TakeDamange(20);
+
+    }
+
+    void TakeDamange(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetMaxHealth(currentHealth);
     }
 }
